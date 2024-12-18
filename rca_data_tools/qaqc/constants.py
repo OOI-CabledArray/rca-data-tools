87d828e96564d091d41134ba8489fdb0ea8f90d2
Wendi Ruef <wruef@uw.edu>
2024-12-18 11:17:05 -0800

    increasing machine size/memory for CE02SHBP VEL

--- rca_data_tools/qaqc/constants.py
+++ rca_data_tools/qaqc/constants.py
@@ -43,9 +43,9 @@ COMPUTE_EXCEPTIONS = {
     },
     'CE02SHBP-LJ01D-07-VEL3DC108':{
         '365': '16vcpu_98gb',
-        '30': '8vcpu_60gb',
-        '7': '8vcpu_60gb',
-        '1': '8vcpu_60gb',
+        '30': '16vcpu_98gb',
+        '7': '16vcpu_98gb',
+        '1': '16vcpu_98gb',
     },
     # ctd
     'CE04OSBP-LJ01C-06-CTDBPO108':{
